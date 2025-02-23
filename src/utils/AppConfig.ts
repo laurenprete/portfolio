50330f18d6c62c646b84e7349a177273b888e20c
Lauren Prete <lprete15@gmail.com>
2025-02-23 02:18:02 -0500

    Update app config meta desc

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,7 +1,6 @@
 export const AppConfig = {
   title: 'Lauren Prete | Software Engineering Professional',
-  description: 'Lauren Prete\'s portfolio - Experienced Full-Stack Software Engineer with 8 years of development expertise.\
-   Specializing in scalable solutions, UX design, frontend, backend, and database management. Passionate about delivering exceptional software. Explore my projects, skills, and experience.',
+  description: 'Full-Stack Software Engineer specializing in scalable solutions, UX design, API development, and database management. Explore my portfolio and projects.',
   author: 'Lauren Prete',
   locale_region: 'en-us',
   locale: 'en',
